59779ca55f6bb44e55d813cc27357d0706a497a1
dtgmwl <34026647+dtgmwl@users.noreply.github.com>
2026-07-02 09:06:15 +0700

    Remove mail notification from TaskCompletedNotification
    remove 'mail'

--- app/Notifications/TaskCompletedNotification.php
+++ app/Notifications/TaskCompletedNotification.php
@@ -17,7 +17,7 @@ class TaskCompletedNotification extends Notification implements ShouldQueue
 
     public function via(object $notifiable): array
     {
-        return ['mail', 'database'];
+        return ['database'];
     }
 
     public function toMail(object $notifiable): MailMessage
